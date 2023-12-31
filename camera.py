1377b2cb1c35c29a6b84da4b8ef68faa84de9a38
Ali <aliamjad.multan@gmail.com>
2023-12-31 11:12:06 +0500

    new

--- camera.py
+++ camera.py
@@ -1,3 +1,3 @@
 print("Here we write the code for camera development")
 print("Code of Gender classification")
-print("Some additional code of camera developer")
+print("Some additional code of camera developers")
